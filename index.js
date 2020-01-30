dfc3211bd06b36d0c213943dd42bd76ee258c51c
Sforzemon <sforzemon@gmail.com>
2020-01-30 18:20:06 -0500

    redoing my node modules...

--- index.js
+++ index.js
@@ -4,25 +4,39 @@ const inquirer = require("inquirer");
 const pdf = require("html-pdf");
 
 
-    
-inquirer
-  .prompt({
-    message: "What is your favorite color?",
-    favoriteColor: "color"
-  })
-  .prompt({
-      message: "what is your Github username?",
-      name: "username"
-  })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}`;
-    axios.get(queryUrl).then(function(res) {
-        // const names = res.data.map(function(repo) {
-        //   return repo.name;
-        return res;
+ console.log("hi there") 
+ inquirer
+  .prompt([
+    {
+      color: 'faveColor',
+      message: 'What is your favorite reptile?',
+    },
+    {
+      name: 'username',
+      message: 'Cool! And what is your Github name?',
+    },
+  ])
+  .then(answers => {
+    console.info('Answer:', answers.username);
+  });  
+// inquirer
+//   .prompt({
+//     message: "What is your favorite color?",
+//     favoriteColor: "color"
+//   })
+//   .prompt({
+//       message: "what is your Github username?",
+//       name: "username"
+//   })
+  // .then(function({ username }) {
+  //   const queryUrl = `https://api.github.com/search/users/${username}`;
+  //   axios.get(queryUrl).then(function(res) {
+  //       // const names = res.data.map(function(repo) {
+  //       //   return repo.name;
+  //       return res;
 
-        });
-        console.log(res);
+  //       });
+  //       console.log(res);
         // const repoNamesStr = repoNames.join("\n");
   
         // fs.writeFile("repos.txt", repoNamesStr, function(err) {
@@ -32,4 +46,4 @@ inquirer
         //   console.log(`Saved ${repoNames.length} repos`);
         // });
     //   });
-    });
+    // });
